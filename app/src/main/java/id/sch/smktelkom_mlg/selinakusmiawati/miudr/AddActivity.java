e2805ec8f6c731a419623c0474d990a8d4c4b57f
Shelin <skusmiawati@gmail.com>
2017-10-17 14:14:43 +0800

    Initialize commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/selinakusmiawati/miudr/AddActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.selinakusmiawati.miudr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
